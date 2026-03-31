e859d88b20b74d0eb7ff21e180195d4e8f51d1b6
sardhex <angelariessp@gmail.com>
2026-03-30 21:16:38 -0500

    test 1: green phase for loan code validation

--- src/main/java/cibertec/RegistroPrestamo.java
+++ src/main/java/cibertec/RegistroPrestamo.java
@@ -6,6 +6,11 @@ public class RegistroPrestamo {
 
     public String registrar(String codigoPrestamo, String codigoLibro, String nombreUsuario, LocalDate fechaPrestamo,
             LocalDate fechaDevolucion) {
-        return "";
+        
+        if (codigoPrestamo == null || !codigoPrestamo.matches("^[A-Z]{2}\\d{4}$")) {
+            return "Ingrese un código de préstamo válido";
+        }
+        
+        return "El préstamo ha sido registrado correctamente";
     }
 }
